cb6f9cdb2a39211bc5264c7b2083a1057846d212
Trevor Whitney <trevorjwhitney@gmail.com>
2012-04-16 14:41:42 -0600

    still fixing stuff from the tag checkout

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,8 +1,1 @@
-# Be sure to restart your server when you modify this file.
-
-Rmpd::Application.config.session_store :cookie_store, key: '_gamez_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# Gamez::Application.config.session_store :active_record_store
+Rmpd::Application.config.session_store :cookie_store, key: '_rmpd_session'
