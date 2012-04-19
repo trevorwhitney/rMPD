852e09697ee5358321f9ae4f71a1960c86cae101
Trevor Whitney <trevorjwhitney@gmail.com>
2012-04-18 19:00:58 -0600

    added some documentation/comments so I know what I did

--- app/assets/javascripts/decaf/client.js
+++ app/assets/javascripts/decaf/client.js
@@ -1,7 +1,16 @@
-//library = { artist : }
+//library is a hash of artists where library[artist] will return
+//a hash of albums by that artist.
+//The album hash is indexed by the albums name, and will return
+//an array of songs. Each song is a hash, as returned by MPD
+//for mor information, see the get_library() function, as well
+//as the parsed json file
 var library = {};
 
+
+//wait for the page to load, and then set everything up here
 $(document).ready(function(){
+  //get the library, which is contained in a cached json file
+  //the complete parameter handles the "loading" screen
   $.ajax({
     url:"/library_objects.json",
     async: false,
@@ -13,6 +22,9 @@ $(document).ready(function(){
       $('div#container').removeClass('hidden');
     }
   });
+
+  //laod the artist and album lists, adding the necessary listeners to
+  //the artists, and defualting to selecting "All" artists
   load_artists(library);
   $('ul#artist_list li:first').addClass('selected');
   load_albums("All");
@@ -23,40 +35,13 @@ $(document).ready(function(){
     artist = $(this).text();
     load_albums(artist);
   });
-  
 });
 
-function clear_selected(string) {
-  if (string == "artists") {
-    $('li.artist').removeClass('selected');
-  }
-  else if (string == "albums") {
-    $('li.album').removeClass('selected');
-  }
-}
-
-function clear_albums() {
-  $('li.album').remove();
-}
-
-function clear_tracks() {
-  $('li.track').remove();
-}
-
-function add_albums_listener() {
-  $('li.album').click(function() {
-    clear_selected('albums');
-    clear_tracks();
-    $(this).toggleClass('selected');
-    album = $(this).text();
-    artist = $('li.artist.selected').text();
-    console.log("Starting loading tracks for" + artist + " " + album);
-    load_tracks(artist, album);
-    console.log("Finished loading tracks...")
-  });
-}
-
-
+//this builds the whole music library into memory. It takes the data object
+//returned by the above ajax call as it's argument. It then parses through this
+//object, which is bascially a hash of the library returned by the server.
+//The library object is populated with artists, which are each a hash of albums,
+//which are each an array of songs. Songs are a hash of attributes.
 function get_library(data) {
   $.each(data.artists, function(i, artist){
     var album_list = {};
@@ -75,6 +60,42 @@ function get_library(data) {
   });
 }
 
+//takes a string as an argument that is either artists, albums, or tracks
+//this function clears all currently selected items in the given context
+function clear_selected(string) {
+  if (string == "artists") {
+    $('li.artist').removeClass('selected');
+  }
+  else if (string == "albums") {
+    $('li.album').removeClass('selected');
+  }
+}
+
+//this removes all entries from the album_list
+function clear_albums() {
+  $('li.album').remove();
+}
+
+//this removes all entries from the track_list
+function clear_tracks() {
+  $('li.track').remove();
+}
+
+//this will add click listeners to each element in the album_list
+//this is abstracted to it's own function since the listeners have to be
+//re-initialized after every time album_list is cleared
+function add_albums_listener() {
+  $('li.album').click(function() {
+    clear_selected('albums');
+    clear_tracks();
+    $(this).toggleClass('selected');
+    album = $(this).text();
+    artist = $('li.artist.selected').text();
+    load_tracks(artist, album);
+  });
+}
+
+//this populates the artist_list from a given library
 function load_artists(_library) {
   var artists = [];
   $.each(_library, function(name, albums) {
@@ -86,6 +107,8 @@ function load_artists(_library) {
   });
 }
 
+//this populates the album_list with albums that belong to a given artist
+//if the artist string is "All", it will add all albums to the list
 function load_albums(artist_string) {
   var _albums = [];
   if (artist_string == "All") {
@@ -108,6 +131,8 @@ function load_albums(artist_string) {
   add_albums_listener();
 }
 
+//this populates the track_list with tracks from a given album
+//if a specific artist is provided, as opposed to all, it will speed things up
 function load_tracks(artist, album) {
   var tracks = {};
   var _tracks = {};
