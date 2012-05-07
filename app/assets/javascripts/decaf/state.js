2e1b7ba04041bf2037c8c0a188426d52daff3b9f
Trevor Whitney <trevorjwhitney@gmail.com>
2012-05-06 21:07:20 -0600

    removed EventSource in favor of WebSockets

--- app/assets/javascripts/decaf/state.js
+++ app/assets/javascripts/decaf/state.js
@@ -1,22 +1,34 @@
-var mpd_server = "http://localhost:3000/";
+var mpd_server = "http://mediacenter:3000/";
 
-var state_es = new EventSource(mpd_server + "state");
-state_es.onmessage = function(e) {
-  switch(e.data) {
-    case "play":
-      $('li#play').addClass('pause');
-      break;
-    case "pause":
-    case "stop":
-      $('li#play').removeClass('pause');
-      break;
-    case "new_song":
-      //load the current song
-      $.ajax({
-        url: mpd_server + 'current_song',
-        dataType: 'json',
-        success: function(data) { update_current_song(data) }
-      });
-      break;
+var timer = new WebSocket("ws://mediacenter:3001/");
+timer.onmessage = function(message) {
+  message = $.parseJSON(message.data);
+  if (message.time != null) {
+    print_time(message.time.elapsed, message.time.total);
+  }
+  if (message.state != null) {
+    switch(message.state) {
+      case "play":
+        $('li#play').addClass('pause');
+        break;
+      case "pause":
+      case "stop":
+        $('li#play').removeClass('pause');
+        break;
+    }
+  }
+  if (message.new_song != null) {
+    $.ajax({
+      url: mpd_server + 'current_song',
+      dataType: 'json',
+      success: function(data) { update_current_song(data) }
+    });
   }
 };
+
+function print_time(elapsed, total) {
+  var minutes = Math.floor(elapsed / 60);
+  var seconds = pad2(elapsed % 60);
+  var percent = Math.floor((elapsed/total)*100);
+  $('#seek_time').text(minutes + ":" + seconds);
+}
