4bd2c1edb899150a095279224c0fb91f1ebdd422
Trevor Whitney <trevorjwhitney@gmail.com>
2012-05-08 15:53:13 -0600

    removed precompiling of assets

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,6 @@ module Rmpd
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile << /^((.*?)\/)?(?!_)[^\/]*$/
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
