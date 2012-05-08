33faea726a589f4f67d3ff7feef952d9de2424ba
Trevor Whitney <trevorjwhitney@gmail.com>
2012-05-08 15:12:56 -0600

    redirect to client on login

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,7 +8,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:success] = "Successfully logged in."
-      redirect_to root_url
+      redirect_to clients_url
     else
       render :action => 'new'
     end
